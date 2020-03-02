4790444b577435010dd17a6d294c646995b5b5f6
kendyjaky@gmail.com <you@example.com>
2020-03-02 11:54:39 +0300

    Modified the icons

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,48 @@ _getNavBar(context){
       )
       
     ),
-
-  ]);
+    Positioned(
+      bottom: 45.0,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildNativeItem(Icons.brightness_3, false),
+          SizedBox(width: 1),
+          _buildNativeItem(Icons.landscape, true),
+          SizedBox(width: 1),
+          _buildNativeItem(Icons.wb_sunny, false),
+          SizedBox(width: 1),
+        ]
+      ),
+    ),
+    Positioned(
+      bottom: 10,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text('Sleep', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight:FontWeight.w800)),
+          SizedBox(width: 1,),
+          Text('Relax', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight:FontWeight.w800)),
+          SizedBox(width: 1,),
+          Text('Be happy', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight:FontWeight.w800)),
+          SizedBox(width: 1,),
+        ],)
+    )
+  ]
+  );
+}
+_buildNativeItem(IconData icon, bool active){
+  return CircleAvatar(
+    radius: 30,
+    backgroundColor: Colors.teal.shade900,
+    child: CircleAvatar(
+      radius: 25,
+      backgroundColor:active? Colors.white.withOpacity(0.9) : Colors.transparent,
+      child: Icon(icon, color: active? Colors.purple: Colors.yellow.withOpacity(0.9)),
+    ),
+  );
 }
 
 class NavBarClipper extends CustomClipper<Path>{
@@ -56,10 +96,13 @@ class NavBarClipper extends CustomClipper<Path>{
     var sw = size.width;
     var sh = size.height;
 
-    path.cubicTo(sw/12, 0, sw/12, 2*sh/5, 2*sw/12, 2*sh/5,);
-    path.cubicTo(3*sw/12, 2*sh/5, 3*sw/12, 0, 4*sw/12, 0);
-
-    
+    path.cubicTo(sw/13, 0, sw/13, 2*sh/5, 2*sw/13, 2*sh/5,);
+    path.cubicTo(3*sw/13, 2*sh/5, 3*sw/13, 0, 4*sw/13, 0);
+    path.cubicTo(5*sw/13, 0, 5*sw/13, 2*sh/5, 6*sw/13, 2*sh/5);
+    path.cubicTo(7*sw/13, 2*sh/5, 7*sw/13, 0, 8*sw/13, 0);
+    path.cubicTo(9*sw/13, 0, 9*sw/13, 2*sh/5, 10*sw/13, 2*sh/5);
+    path.cubicTo(13*sw/13, 2*sh/5, 13*sw/13, 0, 12*sw/13, 0);
+    path.cubicTo(15*sw/13, 0, 15*sw/13, 2*sh/5, 14*sw/13, 2*sh/5);
     path.lineTo(sw, sh);
     path.lineTo(0, sh);
     path.close();
